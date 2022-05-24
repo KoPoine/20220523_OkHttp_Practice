2b730c71adc350e7d77119e65ea6a83152baf76f
조경진 <cho881020@gmail.com>
2022-05-24 14:40:52 +0900

    리싸이클러 뷰 구현  1. 한칸 xml 작성  2. 데이터 클래스 생성  3. 어댑터 뼈대 작성  4. 어댑터 연결 (실데이터 반영 X)  5. 어댑터 실데이터 연결 완료

--- app/src/main/java/com/neppplus/a20220523_okhttp_practice/adapters/TopicRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/a20220523_okhttp_practice/adapters/TopicRecyclerAdapter.kt
@@ -4,21 +4,36 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neppplus.a20220523_okhttp_practice.R
+import com.neppplus.a20220523_okhttp_practice.databinding.TopicListItemBinding
 import com.neppplus.a20220523_okhttp_practice.models.TopicData
 
 class TopicRecyclerAdapter(val mContext : Context, val mList : List<TopicData>) : RecyclerView.Adapter<TopicRecyclerAdapter.MyViewHolder>() {
 
+    lateinit var binding : TopicListItemBinding
+
     inner class MyViewHolder (view : View) : RecyclerView.ViewHolder(view) {
         fun bind(item : TopicData) {
+//            val titleTxt = itemView.findViewById<TextView>(R.id.titleTxt)
+//            val replyCountTxt = itemView.findViewById<TextView>(R.id.replyCountTxt)
+//            val backgroundImg = itemView.findViewById<ImageView>(R.id.backgroundImg)
 
+            binding.titleTxt.text = item.title
+            binding.replyCountTxt.text = "${item.replyCount}명 참여중"
+            Glide.with(mContext)
+                .load(item.imageUrl)
+                .into(binding.backgroundImg)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val row = LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, parent, false)
-        return MyViewHolder(row)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(mContext), R.layout.topic_list_item, parent, false)
+        return MyViewHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
