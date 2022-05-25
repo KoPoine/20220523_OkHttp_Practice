9eacdccc95660e71ac7fc989fa8f6e990b150975
김민철 <kmc901206@gmail.com>
2022-05-25 09:54:58 +0900

    액션바 > 커스텀화  - 여백까지 제거

--- app/src/main/java/com/neppplus/a20220523_okhttp_practice/BaseActivity.kt
+++ app/src/main/java/com/neppplus/a20220523_okhttp_practice/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.a20220523_okhttp_practice
 
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -10,5 +11,18 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    fun setCustomActionBar () {
+//        기본 액션바를 담아줄 변수 생성
+        val defaultActionBar = supportActionBar!!
+
+//        커스텀 모드로 변경 > 우리가 만든 xml로 적용
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
+
+//        양 옆의 여백 제거 -> 모든 영역이 커스텀 뷰
+        val myToolbar = defaultActionBar.customView.parent as androidx.appcompat.widget.Toolbar
+        myToolbar.setContentInsetsAbsolute(0,0)
+    }
 
 }
